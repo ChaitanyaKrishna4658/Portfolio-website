12415db1abdf424891e61aac93d62cc94b4f3a61
G V Chaitanya Krishna <gvchaitanya2005@gmail.com>
2025-06-23 13:01:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const products = [
+    {
+        id: 1,
+        name: "Laptop",
+        price: 50000,
+        description: "A high-performance laptop for all your computing needs.",
+        image: "https://example.com/images/laptop.jpg",
+    },
+    {
+    }
+]
